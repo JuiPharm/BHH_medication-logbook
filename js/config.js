d2420b0f8e13dc3ec51ce2845927e69c40bdb841
JuiPharm <jui_3065@hotmail.com>
2026-05-21 22:13:43 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// ใส่ URL จาก Google Apps Script Deploy
+const CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbydInME7nLpwHxVcTY8RFiiP3Wg7r7Kbvhrbpdu8QDC4lzPDf9rAAZSGOqe-P83K0JD/exec',
+  APP_NAME: 'สมุดยาฝาก'
+};
