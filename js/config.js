1ec1e8aed7dbefb39c99320359dfa9e43235c48b
JuiPharm <jui_3065@hotmail.com>
2026-05-22 13:15:43 +0700

    Update API_URL for deployment

--- js/config.js
+++ js/config.js
@@ -16,7 +16,7 @@
 
 const CONFIG = {
   // 🔴 แก้ URL ตรงนี้หลัง Deploy Google Apps Script
-  API_URL: 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbydInME7nLpwHxVcTY8RFiiP3Wg7r7Kbvhrbpdu8QDC4lzPDf9rAAZSGOqe-P83K0JD/exec',
 
   APP_NAME: 'สมุดยาฝาก',
   VERSION: '1.0.0',
